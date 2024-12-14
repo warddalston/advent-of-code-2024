c43e676bb809e3fd030ed2a12f3de9894e942a0d
Dalston Ward <ward.dalston@gmail.com>
2024-12-13 18:48:18 -0600

    day 4 solution (messy)

--- day-4/day-4.py
+++ day-4/day-4.py
@@ -1,3 +1,6 @@
+###################
+# PART 1 SOLUTION #
+###################
 from typing import NamedTuple
 
 
@@ -41,12 +44,14 @@ directions = [
     Step(1, -1),  Step(1, 0),  Step(1, 1)
 ]
 
-# TODO: Fix the logic in these
+with open("input.txt", 'r') as file:
+    puzzle = file.read().split()
+
 small_puzzle_bounding = {
-    'top': lambda x: x in [0, 1, 2],
-    'left': lambda y: y in [0, 1, 2],
-    'right': lambda y: y in [len(small_puzzle[0]) - x for x in (1, 2, 3)],
-    'bottom': lambda x: x in [len(small_puzzle) - val for val in (1, 2, 3)],
+    'top': lambda x: x not in [0, 1, 2],
+    'left': lambda y: y not in [0, 1, 2],
+    'right': lambda y: y not in [len(puzzle[0]) - x for x in (1, 2, 3)],
+    'bottom': lambda x: x not in [len(puzzle) - val for val in (1, 2, 3)],
     'other': lambda x: True
 }
 
@@ -62,17 +67,53 @@ check_instructions = {
 }
 
 found_words = 0
-for r in range(len(small_puzzle)):
-    for c in range(len(small_puzzle[r])):
-        if small_puzzle[r][c] == 'X':
+for r in range(len(puzzle)):
+    for c in range(len(puzzle[r])):
+        if puzzle[r][c] == 'X':
             print(f'Found X at {r}, {c}! Checking for XMAS!')
             for instruction in check_instructions.values():
-                    if small_puzzle_bounding[instruction.bounds[0]](r):
-                        print('passed row bound check')
-                #     and small_puzzle_bounding[instruction.bounds[1]](c)
-                #     and small_puzzle[r + instruction.step.vertical][c + instruction.step.horizontal] == 'M'
-                #     and small_puzzle[r + (2 * instruction.step.vertical)][c + (2 * instruction.step.horizontal)] == 'A'
-                #     and small_puzzle[r + (3 * instruction.step.vertical)][c + (3 * instruction.step.horizontal)] == 'S'
-                # ):
-                #     found_words += 1
+                if (
+                    small_puzzle_bounding[instruction.bounds[0]](r)
+                    and small_puzzle_bounding[instruction.bounds[1]](c)
+                    and puzzle[r + instruction.step.vertical][c + instruction.step.horizontal] == 'M'
+                    and puzzle[r + (2 * instruction.step.vertical)][c + (2 * instruction.step.horizontal)] == 'A'
+                    and puzzle[r + (3 * instruction.step.vertical)][c + (3 * instruction.step.horizontal)] == 'S'
+                ):
+                    found_words += 1
 print(found_words)
+
+# Now we are looking for X-mas.
+# There are four ways to write an x-mas
+# M . M    M . S    S . M    S . S
+# . A .    . A .    . A .    . A .
+# S . S    M . S    S . M    M . M
+# We need to scan through the input, stopping everytime we find an A to search.
+# We can skip A's in the first row, last row, first column, and last column,
+# As they are never the center of an X.
+# We check the NW first. If it is S, we then check for M in SE. (and vice versa
+# for if NW is M). If a MAS is found, then check in NE and SW. At any failure,
+# continue scanning until the next A.
+
+found_mas = 0
+locations = []
+for r in range(1, len(puzzle) -1):
+    for c in range(1, len(puzzle[r]) - 1):
+        if puzzle[r][c] == 'A':
+            print(f'FOund A at {r}, {c}! Checking for X-MAS!')
+            if (
+                (
+                    (puzzle[r + 1][c + 1] == 'M' and puzzle[r - 1][c - 1] == 'S')
+                    or
+                    (puzzle[r + 1][c + 1] == 'S' and puzzle[r - 1][c - 1] == 'M')
+                )
+                and
+                (
+                    (puzzle[r + 1][c - 1] == 'M' and puzzle[r - 1][c + 1] == 'S')
+                    or
+                    (puzzle[r - 1][c + 1] == 'M' and puzzle[r + 1][c - 1] == 'S')
+                )
+            ):
+                found_mas += 1
+                locations.append((r, c))
+print(found_mas)
+#print(locations)
